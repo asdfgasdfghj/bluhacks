a34cb38b19c0101327ed27033c6ac0d9d0200b44
JesseIguess <blueberry21007@gmail.com>
2024-03-24 21:11:18 +0000

    ukgyfikgjh

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerWidth);
 renderer.antialias = true;
 renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild(renderer.domElement);
+document.getElementById("cont").appendChild(renderer.domElement);
 
 const loader = new THREE.CubeTextureLoader();
 loader.setPath("textures/");
